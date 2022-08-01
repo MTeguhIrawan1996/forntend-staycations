82ffd5bb9f7d5512612fe6767aed3a1fe9d43dd7
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2022-08-01 19:16:38 +0700

    Section Testimonial

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,17 @@
 import HeroImg from "./images/img-hero.jpg";
 import HeroFrame from "./images/img-hero-frame.jpg";
+import TestimonialFrame from "./images/testimonial-landingpages-frame.jpg";
 
 // Icon
 import IcCities from "./images/icons/ic_cities.svg";
 import IcTraveler from "./images/icons/ic_traveler.svg";
 import IcTreasure from "./images/icons/ic_treasure.svg";
 
-export { HeroImg, HeroFrame, IcCities, IcTraveler, IcTreasure };
+export {
+  HeroImg,
+  HeroFrame,
+  TestimonialFrame,
+  IcCities,
+  IcTraveler,
+  IcTreasure,
+};
